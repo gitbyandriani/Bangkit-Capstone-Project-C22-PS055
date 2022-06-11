ee20d601c101e6ffcc3859c420f9c85d37ec698c
Rezazia85 <rezaziau85@gmail.com>
2022-06-12 02:53:33 +0700

    .

--- Cloud_Computing/flask_with_yaml/main.py
+++ Cloud_Computing/flask_with_yaml/main.py
@@ -3,10 +3,9 @@ import uuid
 import flask
 import urllib
 from PIL import Image
-from tensorflow import keras
-from keras.models import load_model
-from keras.utils import load_img, img_to_array
-from flask import Flask, request
+from tensorflow.keras.models import load_model
+from flask import Flask, render_template, request, send_file, jsonify
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
